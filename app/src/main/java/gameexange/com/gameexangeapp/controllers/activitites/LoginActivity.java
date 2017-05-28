55c57b557ddfb36fcab959520389540514f89a3c
guillemerill <guillemerillsoto@gmail.com>
2017-05-28 18:03:09 +0200

    Cambio codigo login + añadir progressbar

--- app/src/main/java/gameexange/com/gameexangeapp/controllers/activitites/LoginActivity.java
+++ app/src/main/java/gameexange/com/gameexangeapp/controllers/activitites/LoginActivity.java
@@ -1,7 +1,11 @@
 package gameexange.com.gameexangeapp.controllers.activitites;
 
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -11,6 +15,8 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import gameexange.com.gameexangeapp.R;
@@ -20,20 +26,24 @@ import gameexange.com.gameexangeapp.models.UserToken;
 
 public class LoginActivity extends AppCompatActivity implements LoginCallback {
 
-    // Elementos de la UI
-    private EditText Username;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
+    private EditText etUsuario;
+    private EditText etPassword;
+    private Button btnLogin;
+    private ProgressBar llProgresslogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Set up the login form.
-        Username = (EditText) findViewById(R.id.user);
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+        etUsuario = (EditText) findViewById(R.id.user);
+        etPassword = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.loginBTN);
+        llProgresslogin = (ProgressBar) findViewById(R.id.login_progress);
+
+
+        // Si en el campo password clica Enter, hace el login
+        etPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.id.password || id == EditorInfo.IME_NULL) {
@@ -44,75 +54,55 @@ public class LoginActivity extends AppCompatActivity implements LoginCallback {
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.loginBTN);
-        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
             }
         });
-        //TODO REGISTRO
 
-//        TextView mRegisterButton = (TextView) findViewById(R.id.registrer_button);
-//        mRegisterButton.setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent i = new Intent(getApplicationContext(), SignUp.class);
-//                startActivity(i);
-//            }
-//        });
+        TextView btnRegistrar = (TextView) findViewById(R.id.registerBTN);
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, RegistroActivity.class);
+               startActivity(i);
+            }
+        });
 
-        mLoginFormView = findViewById(R.id.login_form);
+
+
     }
 
-
-    @Override
-    public void onBackPressed() {
-        // disable going back to the MainActivity
-        moveTaskToBack(true);
-    }
-
-
-    /**
-     * Attempts to log in the account specified by the login form.
-     * If there are form errors (invalid username, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
     private void attemptLogin() {
+        // Quitamos los errores de los TextFields
+        etUsuario.setError(null);
+        etPassword.setError(null);
 
-        Username.setError(null);
-        mPasswordView.setError(null);
-
-        // Store values at the time of the login attempt.
-        String username = Username.getText().toString();
-        String password = mPasswordView.getText().toString();
+        // Cogemos los datos introducidos
+        String usuario = etUsuario.getText().toString();
+        String password = etPassword.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
 
-        // Check for a valid password, if the user entered one.
         if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
+            etPassword.setError(getString(R.string.error_invalid_password));
+            focusView = etPassword;
             cancel = true;
         }
 
-        // Check for a valid username address.
-        if (TextUtils.isEmpty(username)) {
-            Username.setError(getString(R.string.error_field_required));
-            focusView = Username;
+        if (TextUtils.isEmpty(usuario)) {
+            etUsuario.setError(getString(R.string.error_field_required));
+            focusView = etUsuario;
             cancel = true;
         }
 
         if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
             focusView.requestFocus();
         } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            //showProgress(true);
-            LoginManager.getInstance().performLogin(username, password, LoginActivity.this);
+            showProgress(true);
+            LoginManager.getInstance().performLogin(usuario, password, LoginActivity.this);
         }
     }
 
@@ -122,7 +112,7 @@ public class LoginActivity extends AppCompatActivity implements LoginCallback {
 
     @Override
     public void onSuccess(UserToken userToken) {
-        //showProgress(false);
+        showProgress(false);
         Intent intent = new Intent(LoginActivity.this, ProductoListActivity.class);
         startActivity(intent);
         finish();
@@ -132,47 +122,31 @@ public class LoginActivity extends AppCompatActivity implements LoginCallback {
     public void onFailure(Throwable t) {
         Log.e("LoginActivity->", "performLogin->onFailure ERROR " + t.getMessage());
 
+
         // TODO: Gestionar los diversos tipos de errores. Por ejemplo, no se ha podido conectar correctamente.
-        //showProgress(false);
-        mPasswordView.setError(getString(R.string.error_incorrect_username_or_password));
-        mPasswordView.requestFocus();
+        showProgress(false);
+        etPassword.setError(getString(R.string.error_incorrect_username_or_password));
+        etPassword.requestFocus();
     }
 
-    /**
-     * Shows the progress UI and hides the login form.
-     */
-    /*
+    // Progreso del login
     @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
     private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+        if (show) {
             int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
+            btnLogin.setEnabled(false);
 
-            linearLayout.setVisibility(show ? View.VISIBLE : View.GONE);
-            linearLayout.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+            llProgresslogin.setVisibility(View.VISIBLE);
+            llProgresslogin.animate().setDuration(shortAnimTime).alpha(1).setListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                    linearLayout.setVisibility(show ? View.VISIBLE : View.GONE);
+                    llProgresslogin.setVisibility(View.GONE);
                 }
             });
         } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            linearLayout.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            llProgresslogin.setVisibility(View.GONE);
+            btnLogin.setEnabled(true);
         }
     }
-*/
 }
